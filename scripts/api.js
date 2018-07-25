88f94c1fcdb5d9e195e057092961ae5985a9a71f
Ryan Ureta <rcureta95@gmail.com>
2018-07-25 08:28:36 -0700

    merge

---
+++ scripts/api.js
@@ -0,0 +1,7 @@
+const api = (function () {
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/burke-ryan'; const getItems = function(callback) { callback('api module works!'); };
+
+return { getItems };
+
+}());
