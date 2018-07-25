e245614525851b37e32f422673d6ebe8cf6adc04
Ryan Ureta <rcureta95@gmail.com>
2018-07-25 08:42:24 -0700

    finish exercise 2

--- scripts/api.js
+++ scripts/api.js
@@ -4,9 +4,12 @@
 const api = (function () {
 
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/burke-ryan';
-  const getItems = function(callback) {
-    callback('api module works!');
-  };
+
+const getItems = function(callback) {
+    	//callback('api module works!');
+    	$.getJSON(`${BASE_URL}/items`, callback); 
+    };
+ 
   
   return {
     getItems
