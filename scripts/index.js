c879731a96879c7d0cef1ae74497d88080219590
Burkey Koontz <bk.koontz@gmail.com>
2018-07-25 11:25:56 -0700

    finished exercise 7

--- scripts/index.js
+++ scripts/index.js
@@ -16,4 +16,14 @@ $.getJSON('https://thinkful-list-api.herokuapp.com/ei-student/items', (response)
   //console.log('api response:', response);
 });
 
+/*api.getItems((items) => {
+  items.forEach((item) => store.addItem(item));
+  const item = store.items[0];
+  console.log(item);
+  console.log('current name: ' + item.name);
+  store.findAndUpdate(item.id, { name: 'bar' });
+  console.log('new name: ' + item.name);
+});*/
+
+
 
--- scripts/store.js
+++ scripts/store.js
@@ -7,26 +7,31 @@ const store = (function(){
   };
 
   const findById = function(id) {
-    return this.items.find(item => item.id === id);
+    return store.items.find(item => item.id === id);
   };
-
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
+  // ================================================
+  // const findAndToggleChecked = function(id) {
+  //   const item = this.findById(id);
+  //   item.checked = !item.checked;
+  // };
 
   const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
   };
+// ====================================================
+  // const findAndUpdateName = function(id, name) {
+  //   try {
+  //     Item.validateName(name);
+  //     const item = this.findById(id);
+  //     item.name = name;
+  //   } catch(e) {
+  //     console.log('Cannot update name: ' + e.message);
+  //   }
+  // };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
+  const findAndUpdate = function(id, newData) {
+    const item = findById(id);
+    Object.assign(item, newData);
   };
 
   const toggleCheckedFilter = function() {
@@ -41,14 +46,12 @@ const store = (function(){
     items: [],
     hideCheckedItems: false,
     searchTerm: '',
-
     addItem,
     findById,
-    findAndToggleChecked,
     findAndDelete,
-    findAndUpdateName,
     toggleCheckedFilter,
     setSearchTerm,
+    findAndUpdate
   };
   
 }());
